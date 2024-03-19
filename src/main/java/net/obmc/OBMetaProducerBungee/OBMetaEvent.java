d9855ca0829e0101ba49ad30e57955441cb7e481
sean <seanob451@gmail.com>
2024-03-18 23:47:33 -0400

    Switch to FileChannel for writing meta data out

--- src/main/java/net/obmc/OBMetaProducerBungee/OBMetaEvent.java
+++ src/main/java/net/obmc/OBMetaProducerBungee/OBMetaEvent.java
@@ -1,17 +1,19 @@
 package net.obmc.OBMetaProducerBungee;
 
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.ServerConnectRequest;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.event.PostLoginEvent;
-import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.event.ServerSwitchEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -20,49 +22,55 @@ public class OBMetaEvent implements Listener {
 
 	PrintWriter tracker = null;
 	
-	public OBMetaEvent(PrintWriter pw) {
-		tracker = pw;
+	public OBMetaEvent() {
 	}
 	
     @EventHandler
     public void onPostLogin(PostLoginEvent event) {
     	// send message to all players that someone has joined OB
-    	for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
-    		if (event.getPlayer().getName() != player.getName()) {
-    			player.sendMessage(new TextComponent(event.getPlayer().getName() + " has joined the network."));
+    	for ( ProxiedPlayer player : ProxyServer.getInstance().getPlayers() ) {
+    		if ( event.getPlayer().getName() != player.getName() ) {
+    			player.sendMessage( new TextComponent(event.getPlayer().getName() + " has joined the network.") );
     		}
         }
     }
     
     @EventHandler
-    public void onPlayerDisconnect(PlayerDisconnectEvent event) {
+    public void onPlayerDisconnect( PlayerDisconnectEvent event ) {
     	// add event to the tracker
-    	try {
-    		tracker.print("PlayerDisconnectEvent#" + event.getPlayer().getName() + "#ob-" + event.getPlayer().getServer().getInfo().getName() + "#" + getTimestamp() + "\n");
-    		tracker.flush();
-    	} catch (NullPointerException e) {
-    		// catches timeout events
-    	}
+   		String disconnectMsg = "PlayerDisconnectEvent#" + event.getPlayer().getName() + "#ob-" + event.getPlayer().getServer().getInfo().getName() + "#" + getTimestamp();
+   		logTrackerMsg( disconnectMsg );
     }
     
-    @EventHandler
-    public void onServerSwitch(ServerSwitchEvent event) {
+	@EventHandler
+    public void onServerSwitch( ServerSwitchEvent event ) {
     	// add event to the tracker
-    	tracker.print("ServerSwitchEvent#" + event.getPlayer().getName() + "#ob-" + event.getPlayer().getServer().getInfo().getName() + "#" + getTimestamp() + "\n");
-    	tracker.flush();
+    	String switchMsg = "ServerSwitchEvent#" + event.getPlayer().getName() + "#ob-" + event.getPlayer().getServer().getInfo().getName() + "#" + getTimestamp();
+    	logTrackerMsg( switchMsg );
     }  
     
-    /*
-    @EventHandler
-    public void ServerConnectedEvent(ServerConnectedEvent event) {
-    	// add event to the tracker
-    	tracker.print("ServerConnectedEvent:" + event.getPlayer().getName() + ":ob-" + event.getServer().getInfo().getName() + ":" + getTimestamp() + "\n");
-    	tracker.flush();
-    }
-    */
+    private void logTrackerMsg( String msg ) {
+    	
+    	try (RandomAccessFile stream = new RandomAccessFile( OBMetaProducer.metafile, "rw" );
+    		     FileChannel channel = stream.getChannel()) {
+
+    		    // Use tryLock() or lock() to block until the lock is acquired
+    		    FileLock lock = channel.lock();
+    		    try {
+    		        // Move to the end of the file to append data
+    		        channel.position(channel.size());
+    		        // Write data
+    		        channel.write( ByteBuffer.wrap( ( msg + "\n" ).getBytes() ) );
+    		    } finally {
+    		        lock.release();
+    		    }
+    		} catch ( IOException e ) {
+    		    e.printStackTrace();
+    		}		
+	}
     
     private String getTimestamp() {
-    	return new SimpleDateFormat("MM/dd HH:mm:ss.ms").format(new Date());
+    	return new SimpleDateFormat( "MM/dd HH:mm:ss.ms" ).format( new Date() );
 
     }
 }
--- src/main/java/net/obmc/OBMetaProducerBungee/OBMetaProducer.java
+++ src/main/java/net/obmc/OBMetaProducerBungee/OBMetaProducer.java
@@ -1,13 +1,10 @@
 package net.obmc.OBMetaProducerBungee;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 
-import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
@@ -16,8 +13,7 @@ import net.md_5.bungee.config.YamlConfiguration;
 
 public class OBMetaProducer extends Plugin {
 	
-	private String metafile;
-	PrintWriter tracker = null;
+	public static String metafile;
 	Configuration config = null;
 	public static String program = "OBMetaProducerBungee";
 	
@@ -32,16 +28,15 @@ public class OBMetaProducer extends Plugin {
 
 		Register();
 
-		getLogger().info("[" + program + "] Plugin Version " + this.getDescription().getVersion() + " activated");
+		getLogger().info( "[" + program + "] Plugin Version " + this.getDescription().getVersion() + " activated" );
 	
 	}
 
 	@Override
 	public void onDisable() {
 		
-		//close file
-		tracker.close();
-		getLogger().info("[" + program + "] Plugin unloaded");
+		getLogger().info( "[" + program + "] Plugin unloaded" );
+
 	}
 
 
@@ -51,9 +46,9 @@ public class OBMetaProducer extends Plugin {
 	public void LoadConfig() {
 		if (!getDataFolder().exists())
             getDataFolder().mkdir();
-        File file = new File(getDataFolder(), "config.yml");
-        if (!file.exists()) {
-            try (InputStream in = getResourceAsStream("config.yml")) {
+        File file = new File( getDataFolder(), "config.yml" );
+        if ( !file.exists() ) {
+            try (InputStream in = getResourceAsStream( "config.yml" ) ) {
                 Files.copy(in, file.toPath());
             } catch (IOException e) {
                 e.printStackTrace();
@@ -61,37 +56,27 @@ public class OBMetaProducer extends Plugin {
         }
         
 		try {
-			config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
-		} catch (IOException e) {
+			config = ConfigurationProvider.getProvider(YamlConfiguration.class).load( new File(getDataFolder(), "config.yml" ) );
+		} catch ( IOException e ) {
 			e.printStackTrace();
 		}
 	}
 	
 	public void Initialize() {
-		// open our tracker - create if it doesn't exist
-		this.metafile = config.getString("MetaFile.Filename");
-		File file = new File(this.metafile);
+		
+		OBMetaProducer.metafile = config.getString( "MetaFile.Filename" );
+		File file = new File( OBMetaProducer.metafile );
 		if (!file.exists()) {
 			try {
 				file.createNewFile();
-			} catch (IOException e) {
+			} catch ( IOException e ) {
 				e.printStackTrace();
 			}
-		}
-		
-		// open the meta file for writing
-		File mfile = new File(metafile);
-		FileWriter writer;
-		try {
-			writer = new FileWriter(mfile, true);
-			tracker = new PrintWriter(writer);
-		} catch(IOException e) {
-			e.printStackTrace();
 		}
 	}
 
 	public void Register() {
 		// register our event handler
-		getProxy().getPluginManager().registerListener(this, new OBMetaEvent(tracker));
+		getProxy().getPluginManager().registerListener( this, new OBMetaEvent() );
 	}
 }
